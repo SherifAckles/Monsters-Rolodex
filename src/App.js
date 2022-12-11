185f6b6a8858e919829e30bd59037295d06363fa
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-12-11 16:37:20 -0500

    How react render

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ class App extends Component {
     this.state = {
        monsters: [], //initial state value
     };
+    console.log("constructor 1")
   }
   componentDidMount() {
+    console.log("componentDidMount 3")
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => 
       response.json())
       .then((users) => this.setState(() => {
          return {monsters:users}
        }))     
-      
     }
    
 
   render() {
+    console.log("render 2")
     return (
       <div className="App">
         {this.state.monsters.map((monsters) => {
